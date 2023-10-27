2180752fec1541a4ae2cbc4414ba572a23785581
seba <baudinoseba@gmail.com>
2023-10-27 20:14:47 -0300

    Cambio para ejecutar .exe y luego iniciar servidor

--- manage.py
+++ manage.py
@@ -4,10 +4,6 @@ import os
 import sys
 import subprocess
 
-if 'runserver' in sys.argv:
-    # Lanza el archivo go2rtc.exe como un proceso independiente
-    subprocess.Popen(['go2rtc.exe'])
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users_admin.settings')
@@ -19,8 +15,18 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # Verificar si go2rtc.exe existe en el directorio actual
+    go2rtc_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'go2rtc.exe')
+    if not os.path.exists(go2rtc_path):
+        print("Error: 'go2rtc.exe' not found in the directory.")
+        return
+    
+    # Lanzar el archivo go2rtc.exe como un proceso independiente
+    subprocess.Popen([go2rtc_path])
+    
+    # Ejecutar tareas administrativas de Django
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
