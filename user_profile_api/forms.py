53d56b5e6c7b9439827cfaf451a2461af2b384c5
RamrioSclerandi <ramirosclerandi1@gmail.com>
2023-12-21 15:21:40 -0300

    Vista dispositivo indivual

---
+++ user_profile_api/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Device
+
+class DeviceForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Device
+        fields = ['device', 'ip', 'door_port', 'date_purchased', 'is_active', 'is_synchronized', 
+                  'password', 'user', 'massive_opening']  # incluye tus otros campos aquí
