12fa98014c55b531390c8682e3ff9c48cbd3eb8b
te02021 <teoorivero15mail.com>
2023-12-21 15:35:11 -0300

    2

---
+++ user_profile_api/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Device
+
+class DeviceForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Device
+        fields = ['device', 'ip', 'door_port', 'date_purchased', 'is_active', 'is_synchronized', 'user', 'password', 'massive_opening']
+    
+    
